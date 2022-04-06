7bbd2be41e8ad6c8cda0e31477ae2e82d9685207
Crowdbotics <team@crowdbotics.com>
2022-04-06 00:31:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "casino_fun_34617",
+  url: "https://casino_fun_34617.botics.co",
+  api: "https://casino_fun_34617.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
